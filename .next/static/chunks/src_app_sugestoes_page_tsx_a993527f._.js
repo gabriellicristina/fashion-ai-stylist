963f42150db7958cfa193e2d80180dfb9eff599c
Gabrielli <114274029+gabriellicristina@users.noreply.github.com>
2025-07-20 18:16:09 -0300

    Add .next/static/chunks/src_app_sugestoes_page_tsx_a993527f._.js 1753046157893

---
+++ .next/static/chunks/src_app_sugestoes_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_dcf67671._.js",
+  "static/chunks/node_modules_5a6c2778._.js"
+],
+    source: "dynamic"
+});
