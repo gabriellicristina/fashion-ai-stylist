9fa93327b3b927ffd0fa092e08cb626e30aa3beb
Gabrielli <114274029+gabriellicristina@users.noreply.github.com>
2025-07-20 18:16:12 -0300

    Add .next/static/chunks/src_app_cadastro_page_tsx_a993527f._.js 1753046160343

---
+++ .next/static/chunks/src_app_cadastro_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_bb805ec8._.js",
+  "static/chunks/node_modules_05649cdc._.js"
+],
+    source: "dynamic"
+});
